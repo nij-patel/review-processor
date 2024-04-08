60f49e3b530ab647337b68202a147ff16eddb118
Dap185 <dap185@pitt.edu>
2024-04-07 23:27:07 -0400

    clean up

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -82,17 +82,3 @@ function App() {
 
 export default App;
 
-
-        // {/* <a
-        //   className="App-link"
-        //   href="https://reactjs.org"
-        //   target="_blank"
-        //   rel="noopener noreferrer"
-        // >
-        //   Learn React
-        // </a>
-        //  */}
-
-      //    <p>
-      //    Edit <code>src/App.js</code> and save to reload.
-      //  </p>
